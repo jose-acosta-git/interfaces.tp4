20933d1a6d33629cff11b2bb566cd366d7a61843
jose-acosta-git <jose.acosta.git@gmail.com>
2023-06-29 13:08:49 -0300

    Add logout icon

--- js/account.js
+++ js/account.js
@@ -2,6 +2,7 @@
 
 const userText = document.getElementById('userState');
 const accountBtn = document.getElementById('accountBtn');
+const userIcon = document.getElementById('userIcon');
 
 let logged = sessionStorage.getItem('isLogged');
 console.log(logged);
@@ -9,6 +10,7 @@ if (logged == null)
     sessionStorage.setItem('isLogged', 'false');
 if (logged == 'true') {
     userText.textContent = "Cerrar sesión";
+    userIcon.src = 'images/logout.png';
 }
 
 accountBtn.addEventListener('click', (e) => {
