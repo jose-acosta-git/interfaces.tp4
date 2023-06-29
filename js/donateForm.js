81e9d3e749d5b008b04f4e0d89f2a17a1b549cfb
jose-acosta-git <jose.acosta.git@gmail.com>
2023-06-29 13:57:19 -0300

    Add not empty value check in donate form

--- js/donateForm.js
+++ js/donateForm.js
@@ -1,7 +1,12 @@
 const page1 = document.getElementById('page1');
+const page1Btn = document.getElementById('page1-btn');
 const page2 = document.getElementById('page2');
 const backBtn = document.getElementById('back');
 const donateBtn = document.getElementById('donate-button');
+const customInput = document.getElementById('customInput');
+const errorMsg = document.createElement('p');
+errorMsg.id = 'errorMsg';
+let error = false;
 
 page1.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -17,8 +22,18 @@ donateBtn.addEventListener('click', () => {
 });
 
 function nextPage() {
+    if (page1.elements.donation.value == 'custom' && (customInput.value < 0 || customInput.value == "")) {
+        return wrongValue();
+    }
     page1.classList.remove('visible');
     page2.classList.add('visible');
+}
+
+function wrongValue() {
+    errorMsg.innerHTML = "Ingrese un monto mayor a 0";
+    if (!error)
+        page1.insertBefore(errorMsg, page1Btn);
+    error = true;
 }
 
 function previousPage() {
